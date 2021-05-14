46e9c4c886f0babe1f798c308b8ab8f3f40ea7be
rakin406 <rakinhacker@gmail.com>
2021-05-14 16:22:04 +0600

    add todo comment

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::thread::sleep;
 use std::time::Duration;
 
+// TODO: Rotate the tire(this is extremely hard for me, probably requires tons
+// of math). Also move the tire left and right as long as it does not touch
+// the terminal's end side.
 fn main() {
     // TODO: Draw a better version of a tire.
     let mut tire = "
