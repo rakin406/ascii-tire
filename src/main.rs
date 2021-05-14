3cbf48f4f481acafaa4ec387ea1fd8d5d54e6aae
rakin406 <rakinhacker@gmail.com>
2021-05-14 14:15:28 +0600

    add todo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::thread::sleep;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: Draw a better version of a tire.
+    let mut tire = "
+            .-/+oossssoo+/-.
+        `:+sssssssssssssssdMMN:`
+      -+sssssssssssssssssdMMMNys+-
+    .ossssssssssssssssssdMMMNysssso.
+   /ssssssssssshdmmNNmmyNMMMMhssssss/
+  +ssssssssshmydMMMMMMMNddddyssssssss+
+ /sssssssshNMMMyhhyyyyhmNMMMNhssssssss/
+.ssssssssdMMMNhsssssssssshNMMMdssssssss.
++sssshhhyNMMNyssssoooossssyNMMMysssssss+
+yNNNNMMMNyMMhssssoOOOOosssshmmmhssssssso
+yNNNNMMMNyMMhssssoOOOOosssshmmmhssssssso
++sssshhhyNMMNyssssoooossssyNMMMysssssss+
+.ssssssssdMMMNhsssssssssshNMMMdssssssss.
+ /sssssssshNMMMyhhyyyyhdNMMMNhssssssss/
+  +sssssssssdmydMMMMMMMMddddyssssssss+
+   /ssssssssssshdmNNNNmyNMMMMhssssss/
+    .ossssssssssssssssssdMMMNysssso.
+      -+sssssssssssssssssyMMMNys+-
+        `:+ssssssssssssssssMMMN`
+            .-/+oossssoo+/-.
+";
+
+    loop {
+        // TODO: Modify lines of tire
+        for mut line in tire.lines() {
+            line = &(" ".to_owned() + line);
+        }
+
+        print!("\x1B[2J\x1B[1;1H"); // clear terminal screen
+        println!("{}", tire);
+        sleep(Duration::from_millis(1000));
+    }
 }
